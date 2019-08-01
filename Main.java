87da7178dc969b4602003b3ec27fa9c98d9b9cba
Bubb, Matt <mdbubb@users.noreply.github.com>
2019-08-01 15:25:44 -0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,152 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+    public static String[][] board = new String[3][3];
+    public static int[] calculate = new int[9];
+    public static HashMap<String, Integer> name = new HashMap<>();
+    public static ArrayList<Integer> tempListX = new ArrayList<>();
+    public static ArrayList<Integer> tempListY = new ArrayList<>();
+    public static int a = 0;
+    public static boolean gameNotOver() {
+        if (win()) {
+            System.out.println(XorO() + " Wins!");
+        }
+        return !win();
+
+    }
+
+    public static boolean win() {
+        int sum = 0;
+        boolean checkDifference = true;
+        for (int i = 0; i < returnTempList().size(); i++) {
+            sum += returnTempList().get(i);
+        }
+        try {
+            int difference = Math.abs(returnTempList().get(0) - returnTempList().get(1));
+            if (difference == 1) {
+                for (int i = 0; i < returnTempList().size(); i++) {
+                    System.out.println(returnTempList().get(i));
+                }
+                if (returnTempList().contains(2) && returnTempList().contains(4) || returnTempList().contains(5) && returnTempList().contains(7)|| returnTempList().contains(1) && returnTempList().contains(3)|| returnTempList().contains(4) && returnTempList().contains(6)) {
+                    checkDifference = false;
+                }
+            }
+            for (int j = 0; j < returnTempList().size() - 1; j++) {
+                if (Math.abs(returnTempList().get(j) - returnTempList().get(j + 1)) != difference) {
+                    checkDifference = false;
+                }
+            }
+
+        } catch (IndexOutOfBoundsException e) {
+        }
+        return sum % 3 == 0 && returnTempList().size() >= 3 && checkDifference;
+    }
+
+    public static void createBoard(String board[][]) {
+        int b = 0;
+
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                board[i][j] = Integer.toString(i, j);
+                System.out.print("| (" + i + "," + j + ") |");
+                name.put(i + "," + j, b);
+                calculate[b] = b;
+                b++;
+            }
+            System.out.println();
+        }
+
+    }
+
+    public static void getInput() {
+        System.out.println("Player " + XorO() + ", Type the ordered pair of your move ie. 1,1");
+        Scanner input = new Scanner(System.in);
+        String pair = input.next();
+        int x = Integer.parseInt(pair.substring(0, pair.indexOf(",")));
+        int y = Integer.parseInt(pair.substring(pair.indexOf(",") + 1));
+        if (validateInput(board, x, y)) {
+            for (Map.Entry<String, Integer> entry : name.entrySet()) {
+                if (pair.equals(entry.getKey())) {
+                    returnTempList().add(entry.getValue());
+
+                }
+            }
+        }
+        insert(board, x, y);
+    }
+
+    public static ArrayList<Integer> returnTempList() {
+        if (XorO().equals("X")) {
+            return tempListX;
+        } else {
+            return tempListY;
+        }
+    }
+
+    public static boolean validateInput(String board[][], int x, int y) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                if (board[x][y].equals("X") || board[x][y].equals("O")) {
+                    return false;
+                }
+
+            }
+        }
+        return true;
+    }
+
+    public static void insert(String board[][], int x, int y) {
+        if (gameNotOver()) {
+            if (validateInput(board, x, y)) {
+                for (int i = 0; i < board.length; i++) {
+                    for (int j = 0; j < board.length; j++) {
+                        if (i == x && j == y) {
+                            board[i][j] = XorO();
+                        }
+                    }
+                }
+                printBoard(board);
+                a++;
+            } else {
+                System.out.println("POSITION ALREADY TAKEN, CHOOSE ANOTHER POSITION.");
+                printBoard(board);
+                getInput();
+            }
+
+        }
+    }
+    
+    public static void printBoard(String board[][]) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                if (board[i][j].equals("X")) {
+                    System.out.print("    X    ");
+
+                } else if (board[i][j].equals("O")) {
+                    System.out.print("    O    ");
+                } else {
+                    System.out.print("| (" + i + "," + j + ") |");
+
+                }
+            }
+            System.out.println();
+        }
+    }
+
+
+    public static String XorO() {
+        return a % 2 == 0 ? "X" : "O";
+    }
+
+    public static void main(String[] args) {
+        createBoard(board);
+        while (true) {
+            getInput();
+
+
+        }
+    }
+}
